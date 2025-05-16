59808352a9fe08b5199668ce0f297ecda5860bca
CarmineToriello95 <carminetoriello95@gmail.com>
2025-05-16 18:32:14 +0200

    Iplemented local data source.
    Added shared preferences package

--- lib/di_config.dart
+++ lib/di_config.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart';
 import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'di_config.config.dart';
 
@@ -17,4 +18,6 @@ void configureDependencies() => getIt.init();
 abstract class RegisterModule {
   @injectable
   Client get client => Client();
+  @preResolve
+  Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
 }
---
+++ lib/features/prime_number/data/datasources/local_data_source.dart
@@ -0,0 +1,46 @@
+import 'package:clock_app/core/errors/prime_number/prime_number_exceptions.dart';
+import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String _timestampStorageKey = 'timestampSinceEpoch';
+
+abstract class LocalDataSource {
+  Future<bool> savePrimeNumberTimestamp({required DateTime timestamp});
+  Future<DateTime?> retrieveLastPrimeTimestamp();
+}
+
+@Injectable(as: LocalDataSource)
+class LocalDataSourceImpl implements LocalDataSource {
+  final SharedPreferences _sharedPreferences;
+
+  LocalDataSourceImpl(this._sharedPreferences);
+  @override
+  Future<DateTime?> retrieveLastPrimeTimestamp() {
+    try {
+      final int? timestampSinceEpoch = _sharedPreferences.getInt(
+        _timestampStorageKey,
+      );
+      if (timestampSinceEpoch == null) {
+        return Future.value(null);
+      } else {
+        return Future.value(
+          DateTime.fromMillisecondsSinceEpoch(timestampSinceEpoch),
+        );
+      }
+    } catch (e) {
+      throw RetrieveLastPrimeTimestampException(e.toString());
+    }
+  }
+
+  @override
+  Future<bool> savePrimeNumberTimestamp({required DateTime timestamp}) {
+    try {
+      return _sharedPreferences.setInt(
+        _timestampStorageKey,
+        timestamp.millisecondsSinceEpoch,
+      );
+    } catch (e) {
+      throw SaveLastPrimeTimestampException(e.toString());
+    }
+  }
+}
